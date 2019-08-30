09076718fd3970bf7fc61d4fa3bb92bd92c2db8a
xmonoid <micropozitronic@gmail.com>
2019-08-30 10:17:24 +0300

    Corrected launching UI tests. Works properly now.

--- src/test/java/ru/tsystems/ekosykh/gui/MainFrameTest.java
+++ src/test/java/ru/tsystems/ekosykh/gui/MainFrameTest.java
@@ -3,14 +3,14 @@ package ru.tsystems.ekosykh.gui;
 import static org.assertj.swing.finder.WindowFinder.findFrame;
 import static org.assertj.swing.launcher.ApplicationLauncher.application;
 
-import java.awt.*;
-
 import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
 
 import ru.tsystems.ekosykh.Main;
+
+import javax.swing.*;
 
 public class MainFrameTest extends AssertJSwingJUnitTestCase {
 
@@ -19,12 +19,12 @@ public class MainFrameTest extends AssertJSwingJUnitTestCase {
     @Override
     protected void onSetUp() {
         application(Main.class).start();
-        window = findFrame(new GenericTypeMatcher<Frame>(Frame.class) {
-            protected boolean isMatching(Frame frame) {
+        window = findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+            protected boolean isMatching(JFrame frame) {
                 return MainFrame.CAPTURE.equals(frame.getTitle()) && frame.isShowing();
             }
         }).using(robot());
-        window.show();
+        window.focus();
     }
 
     @Override
@@ -40,6 +40,7 @@ public class MainFrameTest extends AssertJSwingJUnitTestCase {
     @Test
     public void testPressZero() {
         window.textBox("display").requireText("0");
+        window.button("1").focus();
         window.button("0").click();
         window.textBox("display").requireText("0");
     }
@@ -47,6 +48,7 @@ public class MainFrameTest extends AssertJSwingJUnitTestCase {
     @Test
     public void testPressOne() {
         window.textBox("display").requireText("0");
+        window.button("1").focus();
         window.button("1").click();
         window.textBox("display").requireText("1");
     }
